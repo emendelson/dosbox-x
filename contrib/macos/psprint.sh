8f4c353bf2689949be54909efaf003f15e5a2f18
emendelson <em36@columbia.edu>
2023-12-19 20:23:53 -0500

    Add no-printer message to print scripts

--- contrib/macos/psprint.sh
+++ contrib/macos/psprint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Get the default printer's name
+default_printer=$(lpstat -d 2>&1)
+# Check if a default printer is set
+if [[ $default_printer == *"no system default destination"* ]]; then
+    message="No default printer set. I cannot print the printer output."
+    # Use osascript to display the message in a dialog box
+	osascript -e "tell app \"System Events\" to display dialog \"$message\" buttons {\"OK\"} with title {\"DOSBox-X Printing\"}"
+	exit
+fi
+
 cd "$(dirname "$0")"
 
 # osascript -e ' tell application "System Events" to display dialog "path is:'"$FULLPATH"' " buttons {"OK"}'
